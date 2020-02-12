aaba43de7b0b2175e15bb8d0be192b3beff5eb3f
The Amazing Sπderman Negro <36167348+CarlosUPC@users.noreply.github.com>
2020-02-12 13:52:31 +0100

    add pylint & testitng opencv

--- AR/CV/test.py
+++ AR/CV/test.py
@@ -1,10 +1,20 @@
-number = 0
-running = True
+import numpy as numpy
+import cv2
 
-while running:
-    number += 1
-    print(number)
 
-    if number == 5:
-        running = False
+cap = cv2.VideoCapture(0)
+
+while(True):
+    ret, frame = cap.read()
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    cv2.imshow('Video frame',gray)
+    if(cv2.waitKey(1) & 0xFF == ord('q')):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
+
+
 
